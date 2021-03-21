16f78b008f48c98e2b029932176a5561e789fb62
João Correia <correia.jafonso@gmail.com>
2021-03-21 18:17:07 +0000

    DB setup (#2)

---
+++ models/todoList/item.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from 'mongoose';
+
+const itemSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('Item', itemSchema);
