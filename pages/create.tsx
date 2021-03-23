e0f8643658df469861025c2c9ec5cc764c725e39
ojaoc <correia.jafonso@gmail.com>
2021-03-23 22:09:24 +0000

    Fixed creation of document, db setup

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,39 +1,26 @@
 import React from 'react';
-import { connectToDatabase } from '@/db';
+import connectToDatabase from '@/db';
 import Item from '@/models/todoList/item';
 
-const create = ({ dbRes }) => <p>{JSON.stringify(dbRes, null, 2)}</p>;
+const Create = ({ dbRes }) => <p>{JSON.stringify(dbRes, null, 2)}</p>;
 
 export async function getServerSideProps() {
-  let result;
+  let dbRes = { success: 'Connected' };
 
-  connectToDatabase()
-    .then(() => {
-      const item = new Item({
-        title: 'First todo',
-        description: 'first todo desc.',
-      });
+  await connectToDatabase();
 
-      item
-        .save()
-        .then((res) => {
-          result = res;
-        })
-        .catch((err) => {
-          result = err;
-        });
-    })
-    .catch(() => {
-      result = { error: 'Connection bigoof' };
-    });
+  //const item = new Item({
+  //  title: 'First todo',
+  //  description: 'first todo desc.',
+  //});
+
+  //await item.save();
 
   return {
     props: {
-      dbRes: result,
+      dbRes,
     },
   };
 }
 
-// The solution is probably here https://stackoverflow.com/questions/62440264/mongoose-nextjs-model-is-not-defined-cannot-overwrite-model-once-compiled
-
-export default create;
+export default Create;
