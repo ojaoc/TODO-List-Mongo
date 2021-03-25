d5e093afebedbec3a355020165e55ec5a9a4acab
João Correia <joao.correia@contisystems.pt>
2021-03-25 13:03:11 +0000

    extraction of components

---
+++ components/ToDoList.tsx
@@ -0,0 +1,24 @@
+import { Box, IconButton, Stack, Text } from '@chakra-ui/react';
+import { GoPlus } from 'react-icons/go';
+import ToDoListItem from './ToDoListItem';
+
+const ToDoList = () => {
+  return (
+    <Box bg="gray.50" maxW="350px" rounded="lg" px="5" py="6">
+      <Text fontSize="lg" fontWeight="semibold">
+        To do
+      </Text>
+      <IconButton
+        w="100%"
+        mt="3"
+        mb="4"
+        aria-label="Add new to do"
+        icon={<GoPlus />}
+      ></IconButton>
+      <Stack direction="column">
+        <ToDoListItem />
+      </Stack>
+    </Box>
+  );
+};
+export default ToDoList;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,10 @@
-import { Box, IconButton, Container, Stack, Text } from '@chakra-ui/react';
-import { GoPlus } from 'react-icons/go';
-import ToDoListItem from '@/components/ToDoListItem';
+import ToDoList from '@/components/ToDoList';
+import { Container } from '@chakra-ui/react';
 
 export default function Home() {
   return (
     <Container maxW="container.lg">
-      <Box bg="gray.50" maxW="350px" rounded="lg" px="5" py="6">
-        <Text fontSize="lg" fontWeight="semibold">
-          To do
-        </Text>
-        <IconButton
-          w="100%"
-          mt="3"
-          mb="4"
-          aria-label="Add new to do"
-          icon={<GoPlus />}
-        ></IconButton>
-        <Stack direction="column">
-          <ToDoListItem />
-        </Stack>
-      </Box>
+      <ToDoList />
     </Container>
   );
 }
